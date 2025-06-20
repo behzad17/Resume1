d424505d587d98b14f2a905f95704ef514e7ab40
Behzad Javadian <behzadjavadian49@gmail.com>
2025-06-20 23:33:10 +0200

    fix sendEmail.js file

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,17 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+  emailjs
+    .send("gmail", "kodax", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
     })
     .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
     );
-    return false;  // To block from loading a new page
+  return false; // To block from loading a new page
 }
