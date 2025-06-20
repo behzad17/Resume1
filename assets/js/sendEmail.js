6005a1b477227eeba826572e365d0776f3eb6602
Behzad Javadian <behzadjavadian49@gmail.com>
2025-06-21 00:04:36 +0200

    fix sendEmail.js and contact,html files

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,6 +1,10 @@
+(function () {
+  emailjs.init("o1pe8dZ9pmT_gqI5G"); // کلید عمومی واقعی شما
+})();
+
 function sendMail(contactForm) {
   emailjs
-    .send("gmail", "kodax", {
+    .send("service_awvo3xf", "template_0w3tgaf", {
       from_name: contactForm.name.value,
       from_email: contactForm.emailaddress.value,
       project_request: contactForm.projectsummary.value,
@@ -8,10 +12,13 @@ function sendMail(contactForm) {
     .then(
       function (response) {
         console.log("SUCCESS", response);
+        alert("Message sent successfully!");
       },
       function (error) {
         console.log("FAILED", error);
+        alert("Failed to send message.");
       }
     );
-  return false; // To block from loading a new page
+  return false; // جلوگیری از reload فرم
 }
+
